d26e31f47a24dd2387a40a7f2edc5abbbd4bb3e9
Nicolas VINCENT <nico.vince@gmail.com>
2022-11-26 09:18:15 +0100

    action: disable debug

--- manage-release.sh
+++ manage-release.sh
@@ -265,7 +265,6 @@ done
 # check gh is logged, this command returns a non-zero exit code when not logged in.
 gh auth status
 
-set -x
 git tag --list
 git branch
 if [ "$(release_exist "${RELEASE_NAME}")" -eq "1" ]; then
